add8a79b7ed76b7b339b8e0be8678781a9bbaeef
Anthony Dampier <Dampieranthony2@gmail.com>
2023-08-10 15:39:36 -0500

    added animation to gauge

--- force-app/main/default/lwc/leadCalcLWC/leadCalcLWC.js
+++ force-app/main/default/lwc/leadCalcLWC/leadCalcLWC.js
@@ -2,16 +2,7 @@ import { LightningElement, track } from 'lwc';
 
 export default class LeadCalcLWC extends LightningElement {
   // Total Interest Score is 1-50. 
-  Score = 60;
-
-  get getScore (){
-    let progressCircle = this.template.querySelector('[data-id="progress-cirle"]');
-      if(progressCircle){
-        this.template.querySelector('[data-id="progress-cirle"]').className=`progress-circle p${this.Score}`;
-        console.log("setProgressCircleClass(): "+ progressCircle);
-      }        
-    return this.Score;
-  }
+  Score = 10;
 
   get getScore(){
     return "p" + this.Score;
