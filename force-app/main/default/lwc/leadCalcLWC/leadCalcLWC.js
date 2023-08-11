570e695b728baae4ef47eaeb6106ddce97e6f1f5
AnthonyDampier <Dampieranthony2@gmail.com>
2023-08-11 08:56:25 -0500

    Radial Progress Bar Ready! 🎉

--- force-app/main/default/lwc/leadCalcLWC/leadCalcLWC.js
+++ force-app/main/default/lwc/leadCalcLWC/leadCalcLWC.js
@@ -1,17 +1,63 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, api, wire } from 'lwc';
+import { getRecord } from "lightning/uiRecordApi";
 
 export default class LeadCalcLWC extends LightningElement {
+  // get recordId
+  @api recordId;
+  @track name;
+  @track email;
+  @track error;
+
+  // not working...
+  @wire(getRecord, {
+    recordId: "$recordId",
+    fields: [NAME_FIELD, EMAIL_FIELD]
+    }) wireuser({
+        error,
+        data
+    }) {
+    if (error) {
+      console.log(error);
+      this.error = error ; 
+    } else if (data) {
+      console.log(data.fields);
+        this.email = data.fields.Email.value;
+        this.name = data.fields.Name.value;
+    }
+}
+
+  get name() {
+    return this.contact.data.fields.Name.value;
+  }
+
+  get title() {
+    return this.contact.data.fields.Title.value;
+  }
+
+  get phone() {
+    return this.contact.data.fields.Phone.value;
+  }
+
+  get email() {
+    return this.contact.data.fields.Email.value;
+  }
+  
   // Total Interest Score is 1-50. 
-  Score = 10;
+  Score = 40;
 
   get getScore(){
     return this.Score;
-  } 
+  }
 
   get constructClass(){
     return "gauge four rischio1" + " " + "p"+this.Score;
   }
-  
-  // Radial Progress Bar
 
+  // Radial Progress Bar
+  Percent = this.Score/50;
+
+  get getStrokeDasharray(){
+    return Math.floor(this.Percent*100) + ", 100";
+  }
+  // Radial Progress Bar
 }
