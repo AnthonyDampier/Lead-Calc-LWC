ad8a936154ef355887fde5fc4ffcbff9ed8ff5e2
Anthony Dampier <Dampieranthony2@gmail.com>
2023-08-15 13:50:05 -0500

    lead calc record changed from Contact to Lead

--- force-app/main/default/lwc/leadCalcLWC/leadCalcLWC.js
+++ force-app/main/default/lwc/leadCalcLWC/leadCalcLWC.js
@@ -1,63 +1,30 @@
 import { LightningElement, track, api, wire } from 'lwc';
-import { getRecord } from "lightning/uiRecordApi";
+import { getRecord, getFieldValue } from "lightning/uiRecordApi";
+import Total_Interest_Score from '@salesforce/schema/Lead.Total_Interest_Score__c';
 
 export default class LeadCalcLWC extends LightningElement {
   // get recordId
   @api recordId;
-  @track name;
-  @track email;
   @track error;
 
-  // not working...
   @wire(getRecord, {
     recordId: "$recordId",
-    fields: [NAME_FIELD, EMAIL_FIELD]
-    }) wireuser({
-        error,
-        data
-    }) {
-    if (error) {
-      console.log(error);
-      this.error = error ; 
-    } else if (data) {
-      console.log(data.fields);
-        this.email = data.fields.Email.value;
-        this.name = data.fields.Name.value;
-    }
-}
+    fields: [Total_Interest_Score]
+  }) 
+  lead;
 
-  get name() {
-    return this.contact.data.fields.Name.value;
+  get totalInterestScore(){
+    return getFieldValue(this.lead.data, Total_Interest_Score);
   }
-
-  get title() {
-    return this.contact.data.fields.Title.value;
-  }
-
-  get phone() {
-    return this.contact.data.fields.Phone.value;
-  }
-
-  get email() {
-    return this.contact.data.fields.Email.value;
-  }
-  
-  // Total Interest Score is 1-50. 
-  Score = 40;
-
-  get getScore(){
-    return this.Score;
-  }
-
-  get constructClass(){
-    return "gauge four rischio1" + " " + "p"+this.Score;
-  }
-
-  // Radial Progress Bar
-  Percent = this.Score/50;
 
   get getStrokeDasharray(){
-    return Math.floor(this.Percent*100) + ", 100";
+    let percentage = Math.floor((getFieldValue(this.lead.data, Total_Interest_Score)/50)*100)
+    return percentage + ", 100";
   }
-  // Radial Progress Bar
+
+  // guage
+    // get constructClass(){
+  //   return "gauge four rischio1" + " " + "p"+this.totalInterestScore();
+  // }
+  // guage
 }
